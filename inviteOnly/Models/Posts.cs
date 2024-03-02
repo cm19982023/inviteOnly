25eb8f9992e28ca3fd834c98e8e0c51dfdc0bbbb
Christian Montana <christian.montana.39@gmail.com>
2024-03-01 21:41:55 -0500

    yes

--- inviteOnly/Models/Posts.cs
+++ inviteOnly/Models/Posts.cs
@@ -1,6 +1,37 @@
-﻿namespace inviteOnly.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Collections.Generic;
+using inviteOnly.Models;
+using inviteOnly.Models;
+
+namespace TabloidFullStack.Models
+
 {
-    public class Posts
+    public class Post
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string CommunityId { get; set; }
+
+        [DisplayName("Header Image URL")]
+        public string Title { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        [DisplayName("Published")]
+        [DataType(DataType.Date)]
+        public DateTime? PublishDateTime { get; set; }
+
+        public bool IsApproved { get; set; }
+
+     
+
+        [DisplayName("Author")]
+        public int UserProfileId { get; set; }
+        public UserProfile? User { get; set; }
     }
 }
